aeb9a1d9dc3e002b0811bccf656c7a0945bbcacd
viperauter <aotaovip@163.com>
2017-05-15 17:19:32 +0800

    change the config

--- autoconfig.sh
+++ autoconfig.sh
@@ -3,15 +3,20 @@ cdir=`pwd`
 #检查是否有git环境
 command -v git >/dev/null 2>&1 || { echo "require git but it's not installed.  Aborting." >&2; exit 1; }
 command -v ctags >/dev/null 2>&1 || { echo "require ctags but it's not installed.  Aborting." >&2; exit 1; }
-#编译需要的vim源码git地址
-#git url(wget):https://github.com/vim/vim/archive/v7.4.144.tar.gz
 
-#if [ ! -f v7.4.144.tar.gz ];then
+#编译需要的vim源码git地址,git上面的7.4.xxx版本不能用
+#git url(wget):https://github.com/vim/vim/archive/v7.4.144.tar.gz
+#ver=7.4.143
+#vimver=v$ver.tar.gz
+#sourceurl=https://github.com/vim/vim/archive/$vimver
+#if [ ! -f $vimver ];then
 #  echo "download the vim source"
-#  wget https://github.com/vim/vim/archive/v7.4.144.tar.gz
+#  #wget https://github.com/vim/vim/archive/v7.4.144.tar.gz
+#  wget $sourceurl -O $vimver
 #fi
-#tar -xzvf v7.4.144.tar.gz
-#mv vim-7.4.144 vim74 7.4.144版本编译出来的没发使用
+#
+#tar -xzvf $vimver
+#mv "vim-$ver" vim74
 
 #编译需要的vim源码git地址ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 if [ ! -f vim-7.4.tar.bz2 ];then
@@ -19,6 +24,7 @@ if [ ! -f vim-7.4.tar.bz2 ];then
   wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 fi
 tar -xjvf vim-7.4.tar.bz2
+
 #编译需要的vimgdb地址
 #git url(wget):https://codeload.github.com/larrupingpig/vimgdb-for-vim7.4/zip/master
 if [ ! -f vimgdb-for-vim7.4-master.zip ];then
@@ -51,7 +57,7 @@ else
   echo "config the vim option error"
 fi
 make install
-cp -rf $cdir/vimgdb-for-vim7.4-master/vimgdb_runtime/ /usr/share/vim/vim/vim74/
+cp -rf $cdir/vimgdb-for-vim7.4-master/vimgdb_runtime/* ~/.vim 
 echo "vim compile success"
 cd /usr/share/vim/vim/vim74/doc/
 echo ":helptags ."|vim
@@ -71,31 +77,27 @@ if [ -f $vimconfname ];then
 fi
   
 touch $vimconfname
-echo -e "if &compatible" >> $vimconfname
-echo -e "  set nocompatible" >> $vimconfname
-echo -e "end" >> $vimconfname
-echo -e "\n" >> $vimconfname
-echo -e "filetype off" >> $vimconfname
-echo -e "set rtp+=~/.vim/bundle/vundle/" >> $vimconfname
-echo -e "call vundle#rc()" >> $vimconfname
-echo -e "\n" >> $vimconfname
-echo -e "\" Let Vundle manage Vundle" >> $vimconfname
-echo -e "Bundle 'gmarik/vundle'" >> $vimconfname
-echo -e "\n" >> $vimconfname
-echo -e "if filereadable(expand(\"~/.vimrc.bundles.local\"))" >> $vimconfname
-echo -e '  source ~/.vimrc.bundles.local' >> $vimconfname
-echo -e "endif" >> $vimconfname
-echo -e "\n" >> $vimconfname
-echo -e "filetype on" >> $vimconfname
-echo -e "set previewheight=10" >>$vimconfname
-echo -e "run macros/gdb_mappings.vim" >>$vimconfname
-echo -e "set asm=0" >>$vimconfname
-echo -e "set gdbprg=gdb" >>$vimconfname
-echo -e "set splitbelow" >>$vimconfname
-echo -e "set splitright" >>$vimconfname
+echo  "if &compatible" >> $vimconfname
+echo  "  set nocompatible" >> $vimconfname
+echo  "end" >> $vimconfname
+echo  "filetype off" >> $vimconfname
+echo  "set rtp+=~/.vim/bundle/vundle/" >> $vimconfname
+echo  "call vundle#rc()" >> $vimconfname
+echo  "\" Let Vundle manage Vundle" >> $vimconfname
+echo  "Bundle 'gmarik/vundle'" >> $vimconfname
+echo  "if filereadable(expand(\"~/.vimrc.bundles.local\"))" >> $vimconfname
+echo  '  source ~/.vimrc.bundles.local' >> $vimconfname
+echo  "endif" >> $vimconfname
+echo  "filetype on" >> $vimconfname
+echo  "set previewheight=10" >>$vimconfname
+echo  "run macros/gdb_mappings.vim" >>$vimconfname
+echo  "set asm=0" >>$vimconfname
+echo  "set gdbprg=gdb" >>$vimconfname
+echo  "set splitbelow" >>$vimconfname
+echo  "set splitright" >>$vimconfname
 
 if [ -f ~/$vimconfname ];then
-  mv ~/.vimrc ~/.vimrc.bak
+    mv ~/.vimrc ~/$".vimrc.$(date +"%Y%m%d%I%M%S").bak"
 fi
 
 cp ./$vimconfname ~/
@@ -107,12 +109,25 @@ if [ ! -f use_vim_as_ide-master.zip ];then
   wget https://codeload.github.com/yangyangwithgnu/use_vim_as_ide/zip/master -O use_vim_as_ide-master.zip
 fi
 unzip use_vim_as_ide-master.zip
-cp ./use_vim_as_ide-master/.vimrc ~/ycm_extra_conf.py
-cp ./use_vim_as_ide-master/.vimrc ~/$vimloadbundle
-cp ./use_vim_as_ide-master/README.md ./
+cd ./use_vim_as_ide-master/
+sed -i "s/^colorscheme solarized/\"colorscheme solarized/" .vimrc 
+sed -i "s/set background=dark/set background=light/" .vimrc
+sed -i "s/^\"colorscheme phd/colorscheme phd/" .vimrc
+cp .vimrc ~/$vimloadbundle
+cp README.md ./../
 #启动vim安装插件
 vim +PluginInstall +qall
+
+#重新配置ycm,从git上下载ubuntu-fix版本
+rm -rf ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/
+git clone https://git.oschina.net/viperauter/ubuntu-fix.git
+cd ~/.vim/bundle/YouCompleteMe/
+command -v cmake>/dev/null 2>&1 || { echo y|apt-get install cmake }
+git submodule update --init --recursive
+./install.py --clang-completer
 echo "auto config vim env success"
+
 #打开README
 vim README.md
 
